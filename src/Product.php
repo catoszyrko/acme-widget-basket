569700ed773148573cf1ebd901e12fa7f3b1818c
yamilszyrko <catoszyrko@gmail.com>
2026-05-08 09:48:30 -0300

    add Product entity

---
+++ src/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Acme;
+
+class Product
+{
+    public function __construct(
+        private string $code,
+        private string $name,
+        private float $price
+    ) {}
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
