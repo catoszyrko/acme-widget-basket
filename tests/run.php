37c26b4ada2dffae72c81d870b90f2d6502032b3
yamilszyrko <catoszyrko@gmail.com>
2026-05-09 23:42:40 -0300

    chore: remove unused run.php file from tests directory

--- tests/run.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-require __DIR__ . '/ProductTest.php';
-echo "\n";
-require __DIR__ . '/BasketTest.php';
-echo "\n";
-echo "All tests executed\n";
